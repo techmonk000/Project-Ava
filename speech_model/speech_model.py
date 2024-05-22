5f8923d8c904a2309b09b136c68ec370a2bfd129
Swarnavo Mukherjee <swarnavomukherjee03@gmail.com>
2024-05-22 13:55:05 +0530

    basics

---
+++ speech_model/speech_model.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import numpy as np 
+import tensorflow as tf 
+import tensorflow
+import keras 
+from keras import layers 
+import matplotlib.pyplot as plt 
+from IPython import display 
+from jiwer import wer 
+
+
+
