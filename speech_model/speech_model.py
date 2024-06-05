2273ad4f24a6d5080d11bb6eada8b86b1a67d58b
techmonk000 <swarnavomukherjee03@gmail.com>
2024-06-05 19:57:00 +0530

    des

--- speech_model/speech_model.py
+++ speech_model/speech_model.py
@@ -1,12 +1,16 @@
 import pandas as pd 
 import numpy as np 
-import tensorflow as tf 
-import tensorflow
-import keras 
+import tensorflow as tf
 from keras import layers 
 import matplotlib.pyplot as plt 
-from IPython import display 
+from IPython import display
 from jiwer import wer 
 
+data_path = "speech_model\dataset\LJSpeech-1.1"
 
+wavs_path = "speech_model\dataset\LJSpeech-1.1\wavs"
+metadata_path = "speech_model\dataset\LJSpeech-1.1\metadata.csv"
 
+metadata_df = pd.read_csv(metadata_path,sep="|",header=None,quoting=3)
+
+print(metadata_df.tail())
